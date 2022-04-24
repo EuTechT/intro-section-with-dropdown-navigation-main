8fe28c75de6fcc85e89092cd11bd87e6f3e03c39
André Filipe <eutech.backup@gmail.com>
2022-04-23 23:09:24 -0300

    :iphone: Mobile menu finished

---
+++ assets/scripts/menuMobile.js
@@ -0,0 +1,15 @@
+let btnMenu = document.querySelector('#btn-menu');
+
+btnMenu.addEventListener('click', () => {
+    let nav = document.querySelector('.navigation');
+    let overlay = document.querySelector('.overlay');
+    
+    btnMenu.classList.toggle('btn-menu--active');
+    nav.classList.toggle('navigation--show');
+    overlay.classList.toggle('overlay--show');
+    overlay.addEventListener('click', () => {
+        btnMenu.classList.remove('btn-menu--active');
+        nav.classList.remove('navigation--show');
+        overlay.classList.remove('overlay--show');
+    });
+})
